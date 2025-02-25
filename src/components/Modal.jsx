cd148daa8e6da76c7fd2439cef7a080a59de1b96
Rohitbanala <o180361@rguktong.ac.in>
2025-02-25 18:36:14 +0530

    while updating data based on location which is asychronus data we have to rerender the location again which causes again fetch location and re render which makes it loop. the useEffect render function after all the components rendered this is used in modal and and implemented effect dependency to open and close dialog based on props

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,20 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import { useEffect, useRef, forwardRef } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+const Modal = forwardRef(function Modal({ open, children, onClose }, ref) {
   const dialog = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
-
+  useEffect(() => {
+    if (open === true) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 });
 
